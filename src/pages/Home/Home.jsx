b4dcdde654c9164316c7f45df1bd5aaa6c6d2769
bruuno1994 <bruuno1015@gmail.com>
2024-08-29 00:16:47 -0300

    Criação do projeto

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,57 @@
+function Home() {
+  return (
+    <div>
+        <h1 class="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Calçados Democrata</h1>
+        <h2 class="text-4xl text-center font-extrabold">Para Homens que Caminham com Confiança</h2>
+        <br />
+      <div class="grid gap-4 m-auto p-5">
+        <div>
+          <img
+            class="h-30 max-w-full rounded-lg m-auto"
+            src="/capa.jpg"
+            alt=""
+          />
+        </div>
+        <div class="grid grid-cols-5 gap-4">
+          <div>
+            <img
+              class="h-30 max-w-full rounded-lg"
+              src="/img1.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-30 max-w-full rounded-lg"
+              src="/img2.webp"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="/img3.webp"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="/img4.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="/img5.webp"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
